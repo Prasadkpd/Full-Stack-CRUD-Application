41cda31915a23a03e47d3b92bc83930436dd3f65
Prasadkpd <2019cs078@stu.ucsc.lk>
2022-07-08 17:24:19 +0530

    backend done

---
+++ backend/routes/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+import {
+    createProduct, deleteProduct,
+    getAllProducts, getProductById, updateProduct,
+} from "../controllers/Product.js";
+
+const router = express.Router();
+
+router.get('/', getAllProducts);
+router.get('/:id', getProductById);
+router.post('/', createProduct);
+router.patch('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+
+export default router;
